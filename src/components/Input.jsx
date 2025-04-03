1e23611162368a788a0bf7b16e228238d49bac76
İlyas SÜZMEN <73710864+isuzmen@users.noreply.github.com>
2025-04-04 02:38:03 +0300

    A comprehensive refactoring process was initiated in the project. First, the repetitive card structure in the Products component was moved into a separate component named Card.jsx and restructured to be used multiple times within Products. This eliminated repetitive JSX code and resulted in a more readable structure. Next, the radio buttons with identical structures in the Category, Price, and Colors components were transformed into a reusable component named Input.jsx. In all three components, the repeated label, input, and .checkmark structures were replaced with the Input component. This reduced code duplication and improved maintainability. The application's entire state management was moved to the App component, treating App like a store and laying the groundwork for passing data via props to the necessary components. Throughout this process, visual consistency was maintained with the help of style files.

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Input = () => {
-  return <div></div>;
+  return (
+    <label className="sidebar-label-container">
+      <input type="radio" name="test" />
+      <span className="checkmark"></span>All
+    </label>
+  );
 };
 
 export default Input;
