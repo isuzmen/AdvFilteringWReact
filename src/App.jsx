87a53e17bc2abc1706deac2b2caa9fbe12a38d89
İlyas SÜZMEN <73710864+isuzmen@users.noreply.github.com>
2025-04-05 14:42:53 +0300

    A dynamic filtering structure was added to the App component, allowing users to filter products based on category and search input. For this purpose, two state variables named selectedCategory and query were defined. The handleInputChange function was created to track changes in the search bar, while handleChange and handleClick functions were implemented to handle selections such as category, price, or color. Products were first filtered based on the search term using filteredItems, then further refined based on category, color, brand, title, new price, or previous price. All of these filtering operations were carried out through the filteredData function, and the resulting products were dynamically listed on the interface by mapping them to the Card component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,66 @@
+import { useState } from 'react';
 import Navigation from './Navigation/Nav';
 import Products from './Products/Products';
 import Recommended from './Recommended/Recommended';
 import Sidebar from './Sidebar/Sidebar';
+import products from './db/data.jsx';
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [query, setQuery] = useState('');
+
+  const handleInputChange = (event) => {
+    setQuery(event.target.value);
+  };
+
+  const filteredItems = products.filter(
+    (product) =>
+      product.title.toLowerCase().indexOf(query.toLocaleLowerCase()) !== -1,
+  );
+
+  const handleChange = (event) => {
+    setSelectedCategory(event.target.value);
+  };
+
+  const handleClick = (event) => {
+    setSelectedCategory(event.target.value);
+  };
+
+  const filteredData = (products, selected, query) => {
+    let filteredProducts = products;
+
+    if (query) {
+      filteredProducts = filteredItems;
+    }
+    if (selected) {
+      filteredProducts = filteredProducts.filter(
+        ({ category, color, company, newPrice, prevPrice, title }) =>
+          category === selected ||
+          color === selected ||
+          company === selected ||
+          title === selected ||
+          newPrice === selected ||
+          prevPrice === selected,
+      );
+    }
+
+    return filteredProducts.map(
+      ({ img, title, star, reviews, prevPrice, newPrice }) => (
+        <Card
+          key={Math.random()}
+          img={img}
+          title={title}
+          star={star}
+          reviews={reviews}
+          prevPrice={prevPrice}
+          newPrice={newPrice}
+        />
+      ),
+    );
+  };
+
+  const result = filteredData(products, selectedCategory, query);
+
   return (
     <>
       <Sidebar />
